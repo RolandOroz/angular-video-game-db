0373eacb741cd99a84ad5a443b43bd28d19e845d
RolandOroz <rolandoroz@gmail.com>
2022-07-27 11:58:25 +0200

    a

--- src/app/components/services/http.service.ts
+++ src/app/components/services/http.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {forkJoin, Observable} from "rxjs";
 import { environment as env } from "src/environments/environment";
 import {APIResponse, Game} from "src/app/models";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +26,31 @@ export class HttpService {
       params: params,
     });
   }
+
+  getGameDetails(id: string): Observable<Game> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
+    const gameTrailersRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/movies`
+    );
+    const gameScreenshotsRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/screenshots`
+    );
+
+    return forkJoin({
+      gameInfoRequest,
+      gameTrailersRequest,
+      gameScreenshotsRequest
+    }).pipe(
+      map((resp: any) => {
+        return {
+          ...resp['gameInfoRequest'],
+          screenshots: resp['gameScreenshotsRequest']?.results,
+          trailers: resp['gameTrailersRequest']?.results,
+        };
+      })
+    );
+  }
+
 }
 
 
